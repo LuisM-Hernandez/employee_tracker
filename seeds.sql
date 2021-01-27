a936ff52f540d9a9f6b4ecda32ede180b128f72f
Luis <m.luishernandez@outlook.com>
2021-01-26 23:04:11 -0500

    added seeds.sql file

---
+++ seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO department
+(name)
+
+VALUES
+
+INSERT INTO role 
+(title, salary, department_id)
+
+
+('Accountant Manager', 75000, 1)
+('Accountant', 70000, 1)
+('Lead Engineer', 130000, 2)
+('Software Engineer', 100000, 2)
+('Legal Team Lead', 150000, 3)
+('Lawyer', 90000, 3)
+('Sales Lead', 45000, 4)
+('Salesperson', 35000, 4)
+
+
+VALUES
+
+INSERT INTO employee
+(first_name, last_name, role_id, manager_id,)
+
+VALUES
